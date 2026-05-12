85c5ca1950d463330c0b60743cf99a0c46f6a535
MrMicek <vojtech.micek.st@vsb.cz>
2026-05-12 17:00:32 +0200

    updated index navbar links for my questions.

--- docs/shared/nav-links.js
+++ docs/shared/nav-links.js
@@ -17,9 +17,12 @@
     "5": "index.html#rs",
     "6": "index.html#rs",
     "7": "index.html#rs",
+    "13": "index.html#pmzs",
     "14": "index.html#pmzs",
     "15": "index.html#ms",
-    "16": "index.html#ms"};
+    "16": "index.html#ms",
+    "17": "index.html#ms",
+    "18": "index.html#ms"};
 
   const allAnchors = document.querySelectorAll(".nav-dropdown a");
   allAnchors.forEach((anchor) => {
